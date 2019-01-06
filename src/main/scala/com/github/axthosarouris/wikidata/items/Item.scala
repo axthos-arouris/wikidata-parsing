d9b8a37fbc9a8091aa72feca937beeca911baa6a
Orestis <orestis.gorgas@gmail.com>
2019-01-06 13:21:43 +0100

    simplify language for easier serializing

--- src/main/scala/com/github/axthosarouris/wikidata/items/Item.scala
+++ src/main/scala/com/github/axthosarouris/wikidata/items/Item.scala
@@ -1,8 +1,7 @@
 package com.github.axthosarouris.wikidata.items
 
-import com.github.axthosarouris.wikidata.WikidataId
-import com.github.axthosarouris.wikidata.items.Language.Language
 import com.github.axthosarouris.wikidata.parsedItems.{ParsedItem, ParsedItemLabel}
+import com.github.axthosarouris.wikidata.{Language, WikidataId}
 
 case class Item(id: WikidataId, labels: Map[Language, String])
 
@@ -10,15 +9,15 @@ object Item {
 
   def fromParsedItem(parsedItem: ParsedItem): Item = {
     val id = parsedItem.id
-    val labels: Map[Language.Value, String] = parsedItem.labels
-      .flatMap { case (_, label) => parseLabel(label) }
+    val labels: Map[Language, String] = parsedItem.labels
+      .map { case (_, label) => parseLabel(label) }
     Item(id, labels)
 
   }
 
   private def parseLabel(label: ParsedItemLabel) = {
-    val lang = Language.parse(label.language)
-    lang.map(l => l -> label.value)
+    val lang = label.language
+    label.language -> label.value
   }
 
 }
--- src/test/scala/com/github/axthosarouris/items/ItemTest.scala
+++ src/test/scala/com/github/axthosarouris/items/ItemTest.scala
@@ -4,8 +4,9 @@ import java.nio.file.Paths
 
 import com.github.axthosarouris.scala.commons.ResourceReader
 import com.github.axthosarouris.wikidata.converters.ItemParser
-import com.github.axthosarouris.wikidata.items.{Item, Language}
+import com.github.axthosarouris.wikidata.items.Item
 import com.github.axthosarouris.wikidata.parsedItems.ParsedItem
+import com.github.axthosarouris.wikidata.supportedLanguages
 import org.scalatest.FlatSpec
 
 class ItemTest extends FlatSpec with ResourceReader {
@@ -18,12 +19,22 @@ class ItemTest extends FlatSpec with ResourceReader {
     "the original json file" in {
     val item: Item = Item.fromParsedItem(parsedItem)
     for {
-      lang <- Language.values
-      langStr = lang.toString()
+      lang <- supportedLanguages
+      langStr = lang.toString
       expected = parsedItem.labels.get(langStr)
     } {
       assert(expected.map(_.value) === item.labels.get(lang))
     }
+  }
+
+  "Item" should "be serialized and deserialized" in {
+    val item = Item.fromParsedItem(parsedItem)
+    val writer = new ItemParser[Item]
+    val json = writer.serialize(item)
+
+    val actualItem = writer.parseString(json)
+    assert(actualItem === item)
+
   }
 
 }
